c270a04a7d1c09c54d51a9180e5dd225650712aa
moonki <moonki0623@naver.com>
2024-03-04 16:35:40 +0900

    feat: Splash Video Added to project

--- App.tsx
+++ App.tsx
@@ -5,8 +5,8 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useState} from "react";
+import type {PropsWithChildren} from "react";
 import {
   SafeAreaView,
   ScrollView,
@@ -15,7 +15,9 @@ import {
   Text,
   useColorScheme,
   View,
-} from 'react-native';
+} from "react-native";
+
+import Video from "react-native-video";
 
 import {
   Colors,
@@ -23,14 +25,14 @@ import {
   Header,
   LearnMoreLinks,
   ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+} from "react-native/Libraries/NewAppScreen";
 
 type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
 function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const isDarkMode = useColorScheme() === "dark";
   return (
     <View style={styles.sectionContainer}>
       <Text
@@ -56,16 +58,37 @@ function Section({children, title}: SectionProps): React.JSX.Element {
 }
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [showSplash, setShowSplash] = useState(true);
+
+  const handleSplashFinish = () => {
+    StatusBar.setHidden(false);
+    setShowSplash(false);
+  };
+  const isDarkMode = useColorScheme() === "dark";
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  if (showSplash) {
+    return (
+      <View style={styles.fullScreen}>
+        <Video
+          source={require("./src/assets/splash/splash_video.mp4")}
+          style={styles.fullScreenVideo}
+          muted={true}
+          repeat={false}
+          resizeMode="cover"
+          onLoad={() => StatusBar.setHidden(true)}
+          onEnd={handleSplashFinish}
+        />
+      </View>
+    );
+  }
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        barStyle={isDarkMode ? "light-content" : "dark-content"}
         backgroundColor={backgroundStyle.backgroundColor}
       />
       <ScrollView
@@ -97,21 +120,33 @@ function App(): React.JSX.Element {
 }
 
 const styles = StyleSheet.create({
+  fullScreen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  fullScreenVideo: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
   },
   sectionTitle: {
     fontSize: 24,
-    fontWeight: '600',
+    fontWeight: "600",
   },
   sectionDescription: {
     marginTop: 8,
     fontSize: 18,
-    fontWeight: '400',
+    fontWeight: "400",
   },
   highlight: {
-    fontWeight: '700',
+    fontWeight: "700",
   },
 });
 
